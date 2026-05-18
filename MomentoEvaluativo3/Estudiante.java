943ddb6e7ad4e162a9bc540161301ca68839a687
Pipe91770 <andres.charrisp@campusucc.edu.co>
2026-05-18 04:46:10 +0000

    Bloque promedio agregado

--- MomentoEvaluativo3/Estudiante.java
+++ MomentoEvaluativo3/Estudiante.java
@@ -3,15 +3,39 @@ package MomentoEvaluativo3;
 import java.util.LinkedList; //Para el historial de las materias
 
 public class Estudiante extends Persona {
-    private int semestreAct; //Semestre actual del estudiante
-    private double[][] notas; //Matriz para almacenar notas por semestre del estudiante
-    private LinkedList<String> historial; //Lista para almacenar el historial de materias cursadas del estudiante
-    
-    public Estudiante(String nombre, String id, String email, int semestreAct){
-        super(nombre, id, email); //Llama al constructor de la clase padre (Persona) para inicializar los atributos heredados
-        this.semestreAct = semestreAct; 
-        this.notas = new double[10][20]; //Matriz de notas con un tamaño de 10 semestres y 20 materias por semestre
-        this.historial = new LinkedList<>(); 
+    private int semestreAct; // Semestre actual del estudiante
+    private double[][] notas; // Matriz para almacenar notas por semestre del estudiante
+    private LinkedList<String> historial; // Lista para almacenar el historial de materias cursadas del estudiante
+
+    public Estudiante(String nombre, String id, String email, int semestreAct) {
+        super(nombre, id, email); // Llama al constructor de la clase padre (Persona) para inicializar los
+                                  // atributos heredados
+        this.semestreAct = semestreAct;
+        this.notas = new double[10][20]; // Matriz de notas con un tamaño de 10 semestres y 20 materias por semestre
+        this.historial = new LinkedList<>();
+    }
+
+    // Método para guardar una calificación específica en la matriz
+    public void registrarNota(int semestre, int materiaIndice, double nota) {
+        this.notas[semestre][materiaIndice] = nota; // Almacena la nota en la posición correspondiente de la matriz
+    }
+
+    // Calcular el promedio de notas del estudiante en un semestre
+    public double calcularPromedio(int semestre) {
+        double suma = 0; // Acumula las calificaciones
+        int contador = 0;
+        for (int i = 0; i < 20; i++) { // 20 debido a que son las materias max del semestre
+            if (notas[semestre][i] != null) { // Comprueba si en la matriz hay una nota
+                suma += notas[semestre][i];
+                contador++;
+            }
+
+        }
+        if (contador == 0) {
+            return 0.0; // Evita división por cero si no hay notas registradas
+        } else {
+            return suma / contador; // Retorna el promedio calculado
+        }
     }
 
     public int getSemestreAct() {
@@ -39,7 +63,7 @@ public class Estudiante extends Persona {
     }
 
     @Override
-    public void mostrarInformacion(){
+    public void mostrarInformacion() {
         System.out.println("\nNombre: " + nombre);
         System.out.println("ID: " + id);
         System.out.println("Email: " + email);
